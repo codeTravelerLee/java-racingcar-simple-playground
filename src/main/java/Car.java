f73b3f44d0cbf6aef57efffd3862c01e963fad35
jxinwoo <12jack030220@gmail.com>
2026-03-25 23:19:29 +0900

    refactor: Car클래스, Random 생성 인터페이스 책임분리 / 매직스트링 상수추출

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -3,18 +3,24 @@ import java.util.Random;
 public class Car {
     private final String name;
     private final StringBuilder moveStatus = new StringBuilder();
+    private static final int MOVE_THRESHOLD = 4;
+    private static final String MOVE_STATUS_MARKER = "-";
+    private final RandomGenerator randomGenerator;
+
     private int totalMoveCount = 0;
     Random random = new Random();
-    public Car(String name) {
+    public Car(String name, RandomGenerator randomGenerator) {
         this.name = name;
+        this.randomGenerator = randomGenerator;
     }
 
     public void move() {
-        int rand = random.nextInt(10);
-        if(rand >= 4) {
-            moveStatus.append("-");
+        int rand = this.randomGenerator.generate();
+        if(rand >= MOVE_THRESHOLD) {
+            moveStatus.append(MOVE_STATUS_MARKER);
             this.totalMoveCount += 1;
             System.out.println(moveStatus);
+
             return;
         }
         System.out.println(moveStatus);
---
+++ src/test/java/CarTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    @Test
+    @DisplayName("4이상의 값이 들어왔을때 자동차가 전진하는지 확인한다");
+    void testCarMoves() {
+    }
+}
