c2bd916b9bcd9b5daddf74bacaeb86ab89812ff1
jxinwoo <12jack030220@gmail.com>
2026-03-26 01:05:55 +0900

    refactor: 메소드 책임 분리

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -8,22 +8,62 @@ public class RacingGame {
     public void run() {
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
-        ArrayList<Car> cars = new ArrayList<>();
+        //자동차의 이름을 입력받는다
         String[] carNamesArray = getValidateCarNames();
+        ArrayList<Car> cars = new ArrayList<>();
         for(String carName: carNamesArray) {
             cars.add(new Car(carName.trim(), randomNumberGenerator ));
         }
 
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int tryCount = scanner.nextInt();
+        //시도 횟수를 입력받는다.
+        int tryCount = getValidateTryCount();
 
-        try {
-            validateTryCountInput(tryCount);
-        } catch(final IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return;
+        printPlayResult(tryCount, cars);
+        printWinner(cars);
+    }
+
+    private String[] getValidateCarNames() {
+        while(true) {
+            try{
+                System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+                String carNames = scanner.nextLine();
+                String[] carNamesArray = carNames.split(",");
+                validateCarNameInput(carNamesArray);
+
+                return carNamesArray;
+            } catch(final IllegalArgumentException e) {
+                System.out.println("에러! :" + e.getMessage());
+            }
         }
+    }
 
+    private int getValidateTryCount() {
+        while(true) {
+            try {
+                System.out.println("시도할 횟수는 몇 회인가요?");
+                int tryCount = scanner.nextInt();
+                validateTryCountInput(tryCount);
+
+                return tryCount;
+            } catch(final IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void validateCarNameInput(String[] carNamesArray) {
+        if(carNamesArray.length < 2) {
+            throw new IllegalArgumentException("쉼표로 구분된 2대 이상의 자동차를 입력해주세요.");
+        }
+    }
+
+    private void validateTryCountInput(int count) {
+        if(count <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 1번 이상이어야 해요!");
+        }
+    }
+
+    private void printPlayResult(int tryCount, ArrayList<Car> cars) {
         System.out.println("실행 결과");
         for(int i=0; i<tryCount; i++) {
             for (Car car : cars) {
@@ -32,7 +72,9 @@ public class RacingGame {
             }
             System.out.println();
         }
+    }
 
+    private void printWinner(ArrayList<Car> cars) {
         ArrayList<String> winners = new ArrayList<>(cars.size());
 
         int highestMoveCount = 0;
@@ -52,32 +94,5 @@ public class RacingGame {
 
         String result = String.join(",", winners) + "가 최종 우승했습니다.";
         System.out.println(result);
-    }
-
-    private String[] getValidateCarNames() {
-        while(true) {
-            try{
-                System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-                String carNames = scanner.nextLine();
-                String[] carNamesArray = carNames.split(",");
-                validateCarNameInput(carNamesArray);
-
-                return carNamesArray;
-            } catch(final IllegalArgumentException e) {
-                System.out.println("에러! :" + e.getMessage());
-            }
-        }
-    }
-
-    private void validateCarNameInput(String[] carNameArray) {
-        if(carNameArray.length < 2) {
-            throw new IllegalArgumentException("쉼표로 구분된 2대 이상의 자동차를 입력해주세요.");
-        }
-    }
-
-    private void validateTryCountInput(int count) {
-        if(count <= 0) {
-            throw new IllegalArgumentException("시도 횟수는 1번 이상이어야 해요!");
-        }
     }
 }
